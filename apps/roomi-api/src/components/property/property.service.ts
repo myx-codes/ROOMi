a54fda79194074f536999e9252ee6ad074c29997
Steven <muhammad95@dgu.ac.kr>
2026-03-12 02:23:19 +0900

    feat: develop getAllPropertiesByAdmin API

--- apps/roomi-api/src/components/property/property.service.ts
+++ apps/roomi-api/src/components/property/property.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose'; // ObjectId ni shu yerdan olish ishonchliroq
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
 import { StatisticModify, T } from '../../libs/types/common';
@@ -208,11 +208,43 @@ export class PropertyService {
 
 
 
+    public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Properties> {
+        const { propertyStatus, propertyLocationList } = input.search;
+        const match: T = {};
+        const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+        if (propertyStatus) match.propertyStatus = propertyStatus;
+        if (propertyLocationList) match.propertyLocation = { $in: propertyLocationList };
+
+        const result = await this.propertyModel
+            .aggregate([
+                { $match: match },
+                { $sort: sort },
+                {
+                    $facet: {
+                        list: [
+                            { $skip: (input.page - 1) * input.limit },
+                            { $limit: input.limit },
+                            lookupMember,
+                            { $unwind: '$memberData' },
+                        ],
+                        metaCounter: [{ $count: 'total' }],
+                    },
+                },
+            ])
+            .exec();
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        return result[0];
+    };
 
 
 
 
-    
+
+
+
+
 }
 
 
