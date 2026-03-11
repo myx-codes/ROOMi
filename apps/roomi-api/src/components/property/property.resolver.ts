17d30958584c6fd0ae9ba582e60fcdd50e0b7066
Steven <muhammad95@dgu.ac.kr>
2026-03-12 02:26:56 +0900

    feat: develop removePropertyByAdmin API

--- apps/roomi-api/src/components/property/property.resolver.ts
+++ apps/roomi-api/src/components/property/property.resolver.ts
@@ -97,7 +97,18 @@ export class PropertyResolver {
         console.log('Mutation: updatePropertyByAdmin');
         input._id = shapeIntoMongoObjectId(input._id);
         return await this.propertyService.updatePropertyByAdmin(input);
-    }
+    };
+
+
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => Property)
+    public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
+        console.log('Mutation: removePropertyByAdmin');
+        const propertyId = shapeIntoMongoObjectId(input);
+        return await this.propertyService.removePropertyByAdmin(propertyId);
+    };
 
 
 
--- apps/roomi-api/src/components/property/property.service.ts
+++ apps/roomi-api/src/components/property/property.service.ts
@@ -266,7 +266,17 @@ export class PropertyService {
         }
 
         return result;
-    }
+    };
+
+
+
+    public async removePropertyByAdmin(propertyId: Types.ObjectId): Promise<Property> {
+        const search: T = { _id: propertyId, propertyStatus: PropertyStatus.DELETE };
+        const result = await this.propertyModel.findOneAndDelete(search).exec();
+        if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+        return result;
+    };
 
 
 
